9a8b07b5cf76eb7a9ec2c26bc44e9b927d031931
ItaloMiguel <italomiguelleite16@gmail.com>
2023-05-11 16:18:02 -0300

    Test com bug: Fiz os teste porem sem exito, não estou conseguindo fazer com o bingResult do spring boot

---
+++ src/test/java/br/com/blog/cursinho/api/account/service/AccountRegisterServiceTest.java
@@ -0,0 +1,88 @@
+package br.com.blog.cursinho.api.account.service;
+
+import br.com.blog.cursinho.api.account.AccountRegisterForm;
+import br.com.blog.cursinho.api.account.AccountRepository;
+import br.com.blog.cursinho.api.role.RoleRepository;
+import br.com.blog.cursinho.shared.domain.Account;
+import br.com.blog.cursinho.shared.domain.Role;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccountRegisterServiceTest {
+
+    @InjectMocks
+    private AccountRegisterService accountRegisterService;
+
+    @Mock
+    private AccountRepository accountRepository;
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private BindingResult bindingResult;
+
+    private Account ACCOUNT;
+    private Role ROLE_USER;
+
+    @BeforeEach
+    void setUp() {
+        ROLE_USER = Role.builder()
+                .id(1L)
+                .name("ROLE_USER")
+                .build();
+
+        ACCOUNT = Account.builder()
+                .id(1L)
+                .email("email@email.com")
+                .password("password")
+                .firstName("firstName")
+                .lastName("lastName")
+                .roles(Set.of(ROLE_USER))
+                .build();
+    }
+
+    @Test
+    void createAccount_ReturnSuccess() {
+        AccountRegisterForm ACCOUNT_REGISTER = AccountRegisterForm.builder()
+                .email("email@email.com")
+                .firstName("fistName")
+                .lastName("lastName")
+                .password("password")
+                .confirmPassword("password")
+                .build();
+
+
+
+        Optional<Role> OPTIONAL_ROLE_USER = Optional.of(ROLE_USER);
+
+        Mockito.when(accountRepository.save(Mockito.any(Account.class))).thenReturn(ACCOUNT);
+        Mockito.when(roleRepository.findByName(Mockito.anyString())).thenReturn(OPTIONAL_ROLE_USER);
+
+        ObjectError error = new ObjectError("senhas", "As senhas não são parecidas.");
+        bindingResult.addError(error);
+
+        ModelAndView response = accountRegisterService.execute(ACCOUNT_REGISTER, bindingResult);
+
+        System.out.println(bindingResult.hasErrors());
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("redirect:/", response.getViewName());
+        Assertions.assertEquals("{}", response.getModel().toString());
+    }
+}
