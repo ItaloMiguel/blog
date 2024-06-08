e67110799248e54996847a7ead6051fb46246e66
ItaloMiguel <italomiguelleite16@gmail.com>
2024-06-08 00:48:35 -0300

    Feat: Adicionando o menu suspenso. Precisa refazer o css o quanto antes

--- src/main/resources/static/js/header/dropDown.js
+++ src/main/resources/static/js/header/dropDown.js
@@ -1,12 +1,16 @@
-const windowProfile = document.getElementById("profile-photo")
+document.addEventListener('DOMContentLoaded', function() {
+    const dropbtn = document.getElementById('dropbtn');
+    const dropdownContent = document.getElementById('dropdown-content');
 
+    dropbtn.addEventListener('click', function() {
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
 
-windowProfile.addEventListener("click", loadDropDown)
-
-function loadDropDown() {
-if (windowProfile.classList.contains("active")) {
-        windowProfile.classList.remove("active")
-    } else {
-        windowProfile.classList.add("active")
-    }
-}
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            if (dropdownContent.style.display === 'block') {
+                dropdownContent.style.display = 'none';
+            }
+        }
+    });
+});
